0d22a8ccd724beb5f17a9f89f0bbd4fa6cddf27b
paradelism <158748366+paradelism@users.noreply.github.com>
2024-04-25 20:38:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int main() {
-    int year;
-    cout << "Sign the year: ";
-    cin >> year;
+    double scale, distance_cm;
 
-    bool leap_year = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+    cout << "Computing the distance between areas." << endl;
 
-    int days = 365 + leap_year;
+    cout << "Enter input data:" << endl;
+    cout << "Map scale (number of kilometers per centimeter) -> ";
+    cin >> scale;
+    cout << "Distance between points representing areas (cm) -> ";
+    cin >> distance_cm;
 
-    cout << "in " << year << " year = " << days << " days" << endl;
+    double distance_km = distance_cm / 100 * scale;
+
+    cout << "Distance between areas: " << distance_km << " km." << endl;
 
     return 0;
 }
