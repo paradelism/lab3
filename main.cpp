338938070d7c57ad4639c9e960d0652bd213911e
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-09 18:38:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 
 int main() {
-    cout << "You, you I see\n";
-    cout << "In my mirror in the morning\n";
-    cout << "Instead of seeing me\n";
-    cout << "I see you\n";
-    cout << "I see your face\n";
-    cout << "And inside me is a growing need\n";
-    cout << "For your embrace\n";
-    cout << "- Author: Marvin Gaye\n"; 
+    cout << "\t\t\tEvery hunter wants to know\n";
+    cout << "\t\t\tWhere to find the beast\n";
+    cout << "\t\t\tBut the one who caught it\n";
+    cout << "\t\t\tIs the one who's stuck at least\n";
     return 0;
 }
