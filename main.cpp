0c5ff725591804c53fafef05869ddde048717195
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-09 18:45:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 
 int main() {
-    cout << "\t\t\tEvery hunter wants to know\n";
-    cout << "\t\t\tWhere to find the beast\n";
-    cout << "\t\t\tBut the one who caught it\n";
-    cout << "\t\t\tIs the one who's stuck at least\n";
+    cout << "Selling a laptop\n\n";
+    cout << "Model: Macbook pro 15 2017\n";
+    cout << "Condition: Excellent\n";
+    cout << "Specifications:\n";
+    cout << "- Processor: Intel Core i7-7700HQ\n";
+    cout << "- RAM: 16 GB LPDDR3\n";
+    cout << "- Storage: 512 GB SSD\n";
+    cout << "- Display: 15.4-inch Retina Display\n";
+    cout << "- Graphics: Radeon Pro 560\n";
+    cout << "- Operating System: macOS Sierra (Upgradable)\n\n";
+    cout << "Additional Information: The laptop has been lightly used and is in excellent condition. No scratches or defects. Comes with original charger.\n\n";
+    cout << "Price: $800\n\n";
+    cout << "Contact Information:\n";
+    cout << "Phone: (+380) 979511220\n";
+    cout << "Location: Vinnitsia, Ukraine\n\n";
+    cout << "**Hurry, this is an exceptional offer!**\n";
     return 0;
 }
