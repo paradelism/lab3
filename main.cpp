52bccc290064ecf6a3e76330d23c62bdf76b9ae0
paradelism <158748366+paradelism@users.noreply.github.com>
2024-05-09 18:34:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
 int main() {
-    double radius;
-
-    cout << "Enter the radius of the sphere: ";
-    cin >> radius;
-
-    double volume = (4.0 / 3.0) * M_PI * pow(radius, 3);
-
-    cout << "Volume of the sphere: " << volume << endl;
-
+    cout << "You, you I see\n";
+    cout << "In my mirror in the morning\n";
+    cout << "Instead of seeing me\n";
+    cout << "I see you\n";
+    cout << "I see your face\n";
+    cout << "And inside me is a growing need\n";
+    cout << "For your embrace\n";
+    cout << "- Author: Marvin Gaye\n"; 
     return 0;
 }
